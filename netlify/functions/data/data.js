329d1c4cf954e35e4e0dab328f04f223ce142f4d
shufschmid <shufschmid@gmail.com>
2023-04-17 21:57:05 +0200

    added real data and icons

--- netlify/functions/data/data.js
+++ netlify/functions/data/data.js
@@ -1,24 +1,32 @@
-const fetch = require('node-fetch')
-exports.handler = async function() {
+const fetch = require("node-fetch");
+exports.handler = async function () {
   try {
-    const response = await fetch('https://cloud.solar-manager.ch/v1/stream/gateway/0000000090480A4F', {
-      headers: { Accept: 'application/json',  'Authorization': `Basic ${process.env.AUTH}` },
-    })
+    const response = await fetch(
+      "https://cloud.solar-manager.ch/v1/stream/gateway/0000000090480A4F",
+      {
+        headers: { Accept: 'application/json',  'Authorization': `Basic ${process.env.AUTH}` },
+        
+      }
+    );
     if (!response.ok) {
       // NOT res.status >= 200 && res.status < 300
-      return { statusCode: response.status, body: response.statusText }
+      return { statusCode: response.status, body: response.statusText };
     }
-    const data = await response.json()
+    const data = await response.json();
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ currentPvGeneration: data.currentPvGeneration }),
-    }
+      body: JSON.stringify({
+        frames: [{ icon: "50",text: data.currentPvGeneration.toString() },
+        { icon: "44432",text: data.currentPowerConsumption.toString() }
+      ],
+      }),
+    };
   } catch (error) {
-    console.log(error) // output to netlify function log
+    console.log(error); // output to netlify function log
     return {
       statusCode: 500,
       body: JSON.stringify({ msg: error.message }), // Could be a custom message or object i.e. JSON.stringify(err)
-    }
+    };
   }
-}
+};
